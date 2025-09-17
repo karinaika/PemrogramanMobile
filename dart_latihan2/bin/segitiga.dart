ac7a75f8f13ab1e1aca8ce83dcec90a751f04f29
karinaika <karinaikaindasa@gmail.com>
2025-09-17 14:06:30 +0700

    Modifikasi

--- dart_latihan2/bin/segitiga.dart
+++ dart_latihan2/bin/segitiga.dart
@@ -1,7 +1,6 @@
 /// Program mencetak pola segitiga bintang menggunakan nested loop.
 void main() {
-
-  // Loop baris dari n ke 1
+  // // Loop baris (dari 5 sampai 1)
   for (int i = 5; i >= 1; i--) {
     String baris = '';
 
@@ -12,7 +11,7 @@ void main() {
 
     print(baris);
   }
-  
+
   // // Loop baris (dari 1 sampai 5)
   // for (int i = 1; i <= 5; i++) {
 }
