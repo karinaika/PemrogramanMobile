39c91664caef0d3195ca14f5c35ca5f7c6520adc
karinaika <karinaikaindasa@gmail.com>
2025-10-01 10:15:40 +0700

    Praktikum 2: Implementasi button row

--- layout_flutter/lib/main.dart
+++ layout_flutter/lib/main.dart
@@ -7,6 +7,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
     // Title Section (judul + lokasi + icon bintang + angka)
     Widget titleSection = Container(
       padding: const EdgeInsets.all(32),
@@ -45,6 +47,16 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    // Praktikum 2: Button Section
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     return MaterialApp(
       title: 'Flutter layout: KARINA IKA 2341760042',
       home: Scaffold(
@@ -53,10 +65,33 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            titleSection, // pakai widget titleSection
+            titleSection,   // dari kode kamu sebelumnya
+            buttonSection,  // tambahan Praktikum 2
           ],
         ),
       ),
+    );
+  }
+
+  // Method helper untuk membuat kolom tombol
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
