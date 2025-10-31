384746aee7bf76208f40d9785dc14b6f1b4f291f
karinaika <karinaikaindasa@gmail.com>
2025-10-31 23:22:44 +0700

    Langkah 3: Tambah method pada model plan.dart

--- master_plan/lib/models/plan.dart
+++ master_plan/lib/models/plan.dart
@@ -3,6 +3,15 @@ import './task.dart';
 class Plan {
   final String name;
   final List<Task> tasks;
-  
+
   const Plan({this.name = '', this.tasks = const []});
+
+  // Hitung jumlah task yang sudah selesai
+  int get completedCount => tasks
+      .where((task) => task.complete)
+      .length;
+
+  // Pesan ringkasan jumlah task
+  String get completenessMessage =>
+      '$completedCount out of ${tasks.length} tasks';
 }
